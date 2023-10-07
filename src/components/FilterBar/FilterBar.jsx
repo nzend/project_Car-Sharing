1e543289362e27482b7dc13f93c45aef9101bc9d
nzend <115723110+nzend@users.noreply.github.com>
2023-10-07 03:24:58 +0300

    add styles for filter

--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
 import axios from 'axios';
-import { FilterForm, FilterLabel } from './FilterBar.styled';
+import {
+  FilterForm,
+  FilterLabel,
+  SubmitBtn,
+  InputMilageWrapper,
+  InputMilageMin,
+  InputMilageMax,
+} from './FilterBar.styled';
 
 const brandOptions = [
   { value: 'Toyota', label: 'Toyota' },
@@ -9,11 +16,47 @@ const brandOptions = [
   { value: 'Ford', label: 'Ford' },
 ];
 
+const selectStyles = {
+  control: provided => ({
+    ...provided,
+    border: 'none',
+    borderRadius: '14px',
+    backgroundColor: '#F7F7FB',
+    width: '224px',
+    padding: '4px 14px 4px 18px',
+    fontSize: '16px',
+    height: "48px",
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    height: '48px',
+    backgroundColor: state.isFocused ? 'white' : 'white',
+    color: state.isFocused ? '#121417' : ' rgba(18, 20, 23, 0.20);',
+    fontFamily: 'Manrope',
+    fontSize: '16px',
+    cursor: 'pointer',
+  }),
+  placeholder: provided => ({
+    ...provided,
+    fontWeight: '500',
+    color: '#121417',
+    fontFamily: 'Manrope',
+    fontSize: '18px',
+    lineHeight: '20px',
+  }),
+  menu: provided => ({
+    ...provided,
+    borderRadius: '14px',
+    padding: '14px 8px 14px 18px',
+  }),
+};
+
 const FilterBar = () => {
   const [cars, setCars] = useState([]);
   const [selectedBrand, setSelectedBrand] = useState(null);
   const [selectedPrice, setSelectedPrice] = useState(null);
-  const [selectedMileage, setSelectedMileage] = useState(null);
+  const [selectedMileageMin, setSelectedMileageMin] = useState(null);
+  const [selectedMileageMax, setSelectedMileageMax] = useState(null);
 
   useEffect(() => {
     async function fetchCars() {
@@ -39,8 +82,11 @@ const FilterBar = () => {
     setSelectedPrice(selectedOption);
   };
 
-  const handleMileageChange = selectedOption => {
-    setSelectedMileage(selectedOption);
+  const handleMileageChangeMin = selectedOption => {
+    setSelectedMileageMin(selectedOption);
+  };
+  const handleMileageChangeMax = selectedOption => {
+    setSelectedMileageMax(selectedOption);
   };
 
   const handleLoadCarsSubmit = async e => {
@@ -52,7 +98,12 @@ const FilterBar = () => {
           params: {
             brand: selectedBrand ? selectedBrand.value : undefined,
             price: selectedPrice ? selectedPrice.value : undefined,
-            mileage: selectedMileage ? selectedMileage.value : undefined,
+            mileageMin: selectedMileageMin
+              ? selectedMileageMin.value
+              : undefined,
+            mileageMax: selectedMileageMax
+              ? selectedMileageMax.value
+              : undefined,
           },
         }
       );
@@ -65,65 +116,55 @@ const FilterBar = () => {
 
   return (
     <FilterForm onSubmit={handleLoadCarsSubmit}>
-      <FilterLabel>
-        Car brand
-        <Select
-          value={selectedBrand}
-          onChange={handleBrandChange}
-          options={brandOptions}
-          isClearable
-          placeholder="Enter the text"
-        />
-      </FilterLabel>
+      <div>
+        <FilterLabel>Car brand</FilterLabel>
+        <InputMilageWrapper>
+          <Select
+            value={selectedBrand}
+            onChange={handleBrandChange}
+            options={brandOptions}
+            isClearable
+            placeholder="Enter the text"
+            styles={selectStyles}
+          />
+        </InputMilageWrapper>
+      </div>
+      <div>
+        <FilterLabel>Price/ 1 hour</FilterLabel>
+        <InputMilageWrapper>
+          <Select
+            value={selectedPrice}
+            onChange={handlePriceChange}
+            options={[
+              { value: '20', label: '20' },
+              { value: '22', label: '22' },
+              { value: '25', label: '25' },
+              // Додайте більше варіантів цін
+            ]}
+            isClearable
+            placeholder="To $"
+            styles={selectStyles}
+            
+          />
+        </InputMilageWrapper>
+      </div>
+      <div>
+        <FilterLabel>Сar mileage / km</FilterLabel>
+        <InputMilageWrapper>
+          <InputMilageMin
+            onChange={handleMileageChangeMin}
+            placeholder="From"
+            styles={selectStyles}
+          />
+          <InputMilageMax
+            onChange={handleMileageChangeMax}
+            placeholder="to"
+            styles={selectStyles}
+          />
+        </InputMilageWrapper>
+      </div>
 
-      <FilterLabel>
-        Price/ 1 hour
-        <Select
-          value={selectedPrice}
-          onChange={handlePriceChange}
-          options={[
-            { value: '20', label: '20' },
-            { value: '22', label: '22' },
-            { value: '25', label: '25' },
-            // Додайте більше варіантів цін
-          ]}
-          isClearable
-          placeholder="To $"
-        />
-      </FilterLabel>
-
-      <FilterLabel>
-        Сar mileage / km
-        <Select
-          value={selectedMileage}
-          onChange={handleMileageChange}
-          options={[
-            { value: '50000', label: '50000' },
-            { value: '55000', label: '55000' },
-            { value: '60000', label: '60000' },
-            // Додайте більше варіантів пробігу
-          ]}
-          isClearable
-          placeholder="From"
-        />
-      </FilterLabel>
-      <FilterLabel>
-        Сar mileage / km
-        <Select
-          value={selectedMileage}
-          onChange={handleMileageChange}
-          options={[
-            { value: '50000', label: '50000' },
-            { value: '55000', label: '55000' },
-            { value: '60000', label: '60000' },
-            // Додайте більше варіантів пробігу
-          ]}
-          isClearable
-          placeholder="to"
-        />
-      </FilterLabel>
-
-      <button type="submit">Завантажити авто</button>
+      <SubmitBtn type="submit">Search</SubmitBtn>
     </FilterForm>
   );
 };
--- src/components/FilterBar/FilterBar.styled.jsx
+++ src/components/FilterBar/FilterBar.styled.jsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 export const FilterForm = styled.form`
   display: flex;
   justify-content: center;
+  align-items: center;
   gap: 18px;
 `;
 
@@ -10,7 +11,39 @@ export const FilterLabel = styled.label`
   color: #8a8a89;
 
   font-size: 14px;
-  
+
   font-weight: 500;
-  line-height: 1.28; 
+  line-height: 1.28;
+`;
+
+export const SubmitBtn = styled.button`
+  border-radius: 12px;
+  background: #3470ff;
+  color: #fff;
+  padding: 14px 44px;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 1.42;
+`;
+
+export const InputMilageWrapper = styled.div`
+  display: flex;
+`;
+
+export const InputMilageMin = styled.input`
+  border: none;
+  border-radius: 14px 0px 0px 14px;
+  border-right: 1px solid rgba(138, 138, 137, 0.2);
+  background: #f7f7fb;
+  width: 160px;
+
+  padding: 14px 93px 14px 24px;
+`;
+export const InputMilageMax = styled.input`
+  border: none;
+  border-radius: 0px 14px 14px 0px;
+  background: #f7f7fb;
+  width: 160px;
+
+  padding: 14px 115px 14px 24px;
 `;
