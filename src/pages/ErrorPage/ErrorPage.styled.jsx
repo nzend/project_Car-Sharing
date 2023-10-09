3222edc07a51ab9f3578db5fe99af20099c224c1
nzend <115723110+nzend@users.noreply.github.com>
2023-10-10 02:00:40 +0300

    Add 404 page

---
+++ src/pages/ErrorPage/ErrorPage.styled.jsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+
+export const Title = styled.span`
+  margin-bottom: 14px;
+
+  color: #fff;
+
+  font-size: 120px;
+  font-weight: 700;
+  line-height: 1;
+  letter-spacing: 0.66px;
+`;
+
+export const Text = styled.span`
+  margin-bottom: 28px;
+  width: 520px;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.28;
+`;
